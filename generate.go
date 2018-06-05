e026a504c33d34b47d934700211c0ce2627764a4
Bas van Beek <bas@tobin.nl>
2018-06-05 09:38:18 +0200

    moved cli project from services to clients

--- generate.go
+++ generate.go
@@ -3,7 +3,7 @@ package ocgokitexample
 //go:generate protoc -I$GOPATH/src -I. services/device/transport/pb/svcdevice.proto --go_out=plugins=grpc:.
 //go:generate protoc -I$GOPATH/src -I. services/qr/transport/pb/qr.proto --go_out=plugins=grpc:. --twirp_out=.
 //go:generate protoc -I$GOPATH/src -I. services/event/transport/pb/event.proto --go_out=plugins=grpc:. --twirp_out=.
-//go:generate go build -tags sqlite3 -o build/cli services/cli/main.go
+//go:generate go build -tags sqlite3 -o build/cli clients/cli/main.go
 //go:generate go build -tags sqlite3 -o build/ocg-elegantmonolith services/elegantmonolith/main.go
 //go:generate go build -tags sqlite3 -o build/ocg-event services/event/cmd/main.go
 //go:generate go build -tags sqlite3 -o build/ocg-qrgenerator services/qr/cmd/main.go
